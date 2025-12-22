24b8a99866278c9099e2942593da42b228c267e5
gilaineyo <gilaine.young@education.gov.uk>
2025-12-22 14:09:04 +0000

    adds basic input validation function and test file

---
+++ test_main.py
@@ -0,0 +1,10 @@
+from main import is_positive_integer
+
+def is_positive_integer_happy():
+    assert is_positive_integer("2") == True
+    assert is_positive_integer("27") == True
+
+def test_validate_input_unhappy():
+    assert is_positive_integer("not a num") == False
+    assert is_positive_integer("-2") == False
+    assert is_positive_integer("0") == False
