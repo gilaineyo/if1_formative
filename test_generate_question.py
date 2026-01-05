3326cd8827af73ed130d192d65b9d5fa83f4b954
gilaineyo <gilaine.young@education.gov.uk>
2026-01-05 18:06:56 +0000

    refactor generate_question, update tests and add randint mocking

--- test_generate_question.py
+++ test_generate_question.py
@@ -1,29 +1,38 @@
-from generate_question import generate_question, create_division, create_multiplication, create_addition, create_subtraction # Import the question generator and question type functions
+import random
+from generate_question import generate_question # Import the question generator and question type functions
 
-def test_create_division():
-    result = create_division(3, 5) 
+def test_generate_division(monkeypatch):
+    values = [3,5,30,50]
+    monkeypatch.setattr(random, 'randint', lambda x, y: values.pop(0))
+    result = generate_question("division") 
     assert isinstance(result["answer"], int)
     assert isinstance(result["question_text"], str)
     assert result["answer"] == 5
     assert result["question_text"] == f"What is 15 divided by 3?\n"
 
-def test_create_multiplication():
-    result = create_multiplication(3,5)
+def test_generate_multiplication(monkeypatch):
+    values = [3,5,30,50]
+    monkeypatch.setattr(random, 'randint', lambda x, y: values.pop(0))
+    result = generate_question("multiplication") 
     assert isinstance(result["answer"], int)
     assert isinstance(result["question_text"], str)
     assert result["answer"] == 15
     assert result["question_text"] == f"What is 3 multiplied by 5?\n"
 
-def test_create_addition():
-    result = create_addition(3,5)
+def test_generate_addition(monkeypatch):
+    values = [3,5,30,50]
+    monkeypatch.setattr(random, 'randint', lambda x, y: values.pop(0))
+    result = generate_question("addition") 
     assert isinstance(result["answer"], int)
     assert isinstance(result["question_text"], str)
-    assert result["answer"] == 8
-    assert result["question_text"] == f"What is 3 plus 5?\n"
+    assert result["answer"] == 80
+    assert result["question_text"] == f"What is 30 plus 50?\n"
 
-def test_create_subtraction():
-    result = create_subtraction(3,5)
+def test_generate_subtraction(monkeypatch):
+    values = [3,5,30,50]
+    monkeypatch.setattr(random, 'randint', lambda x, y: values.pop(0))
+    result = generate_question("subtraction") 
     assert isinstance(result["answer"], int)
     assert isinstance(result["question_text"], str)
-    assert result["answer"] == 5
-    assert result["question_text"] == f"What is 8 minus 3?\n"
+    assert result["answer"] == 50
+    assert result["question_text"] == f"What is 80 minus 30?\n"
